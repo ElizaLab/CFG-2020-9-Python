aafd646e9244eb0c68cb56565c9a0d81d8b3b220
Filipe Paiva <filipe.p@soltroiavillas.com>
2020-09-21 11:57:06 +0100

    adding homework that I prepared in the weekend

---
+++ homework2.1.py
@@ -0,0 +1,24 @@
+###EXERCISE 1 - Play with list!
+
+# Define a list food for easter (at least 5 items)
+
+# print all of the list
+
+# Prompt the user for something to add to the list
+
+# add it to the end of the list (use append)
+
+# print item index 3 in the list
+
+# print the last item on the list (there is a trick to always print)
+
+
+###Exercise 2 - use a for loop on list to print a string + object inside!
+
+# Iterate using a for loop and shout in all caps everything you and your loved ones will eat.
+# example: "OMG WE WILL HAVE SO MUCH <item>"
+
+
+
+
+
---
+++ homework2.2.py
@@ -0,0 +1,18 @@
+### Exercise 2.2 functions-
+
+# 2.2.1 - Fix this function
+   # this function should print "hello human!"
+# def print_hello_human():
+    #print("WHAT?")
+
+# 2.2.3 - Fix function full_name
+    # this funtion should return the full name
+# def full_name(f_name, l_name):
+    #print("WHAT?")
+
+
+# 2.2.3 - Fix function
+    # this funtion should take in an argument and shout it back at us in a string saying: I WANT <FOOD_ITEM>
+
+# def shout_i_want(fodd):
+    #print("WHAT?")
